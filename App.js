a3b7e996b7770ae2838429458e54dcc51cf1acb5
matttavaares9 <145163424+matttavaares9@users.noreply.github.com>
2024-02-27 17:40:40 -0500

    Updated App.js to handle a transition between screens
    Created two files called HomeScreen.js and TitleScreen.js, and App.js transitions between the two screens using React Native's Nativigator.

--- App.js
+++ App.js
@@ -1,38 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './HomeScreen';
+import TitleScreen from './TitleScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>FANTASY ULTIMATE TEAM</Text>
-      <TouchableOpacity style={styles.buttonContainer} onPress={() => console.log('Hello World!')}>
-        <Text style={styles.buttonText}>PLAY NOW!</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="TitleScreen"
+        screenOptions={{
+          gestureEnabled: false,
+          headerShown: false,
+          ...TransitionPresets.ModalFadeTransition // Apply a slide from right transition
+        }}
+      >
+        <Stack.Screen name="TitleScreen" component={TitleScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color: "#D8AF1F",
-  },
-  buttonContainer: {
-    marginTop: 20,
-    backgroundColor: '#e0e0e0',
-    padding: 10,
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'black', // Gold color of the text
-    textAlign: 'center', // Center align the text within the TouchableOpacity
-  },
-});
+export default App;
