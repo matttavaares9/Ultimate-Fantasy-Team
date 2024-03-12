1b5acba7ccc1a7498b0ef91dd66aa2c0e60b07c4
matttavaares9 <145163424+matttavaares9@users.noreply.github.com>
2024-03-12 17:13:55 -0400

    Update App.js
    Added navigation to Bench screen.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStackNavigator, TransitionPresets } from '@react-navigation/stack
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
 import TitleScreen from './TitleScreen';
+import Bench from './Bench';
 
 const Stack = createStackNavigator();
 
@@ -17,8 +18,10 @@ const App = () => {
           ...TransitionPresets.ModalFadeTransition // Apply a slide from right transition
         }}
       >
+        <Stack.Screen name="Bench" component={Bench} />
         <Stack.Screen name="TitleScreen" component={TitleScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
