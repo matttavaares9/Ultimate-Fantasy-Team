23f85519f6534f23f6cad33149c9c18f3047b911
Dtzaka <63433619+TzakaTech@users.noreply.github.com>
2024-04-17 20:34:57 -0400

    Update App.js
    added it so we can scroll between screens

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
+// App.js
+
 import React from 'react';
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
-import TitleScreen from './TitleScreen';
-import Bench from './Bench';
 import Packs from './Packs';
 import Matchup from './Matchup';
 import League from './League';
-import getPacks from './getPacks';
-import Sets from './Sets';
-import Challenges from './Challenges';
+import TitleScreen from './TitleScreen';
 
+const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
+
+function SwipeableTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      tabBarOptions={{ style: { height: 0 } }}
+      swipeEnabled={true}
+    >
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="Packs" component={Packs} />
+      <Tab.Screen name="Matchup" component={Matchup} />
+      <Tab.Screen name="League" component={League} />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="TitleScreen"
-        screenOptions={{
-          gestureEnabled: false,
-          headerShown: false,
-          ...TransitionPresets.ModalFadeTransition // Apply a slide from right transition
-        }}
-      >
-        <Stack.Screen name="Bench" component={Bench} />
+      <Stack.Navigator initialRouteName="TitleScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="TitleScreen" component={TitleScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="Packs" component={Packs} />
-        <Stack.Screen name="Matchup" component={Matchup} />
-        <Stack.Screen name="League" component={League} />
-        <Stack.Screen name="getPacks" component={getPacks} />
-        <Stack.Screen name="Sets" component={Sets} />
-        <Stack.Screen name="Challenges" component={Challenges} />
-
+        <Stack.Screen name="MainTabs" component={SwipeableTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
